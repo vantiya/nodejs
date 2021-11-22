1b830c86c6bf6bf11556118f1d34ba01ad039b2a
vantiya <parvezvantiya@gmail.com>
2021-11-22 19:27:42 +0530

    Added dotenv module and implement mongoDB cloud connection from env file

--- nodeAPI/server.js
+++ nodeAPI/server.js
@@ -1,7 +1,12 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+const env = require("./config.env");
 
+dotenv.config({ path: "./config.env" });
+const port = process.env.PORT || 3000;
 // Start the server
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server started at http://127.0.0.1:${port}`);
+console.log(process.env);
+app.listen(3000, () => {
+    console.log(`Server started at http:127.0.0.1:3000`);
+    // console.log(`Server started at ${process.env.HOST}:${port}`);
 });
