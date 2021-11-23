83fd78ab46c86eb25e2bf3956bc663441658fbc4
vantiya <parvezvantiya@gmail.com>
2021-11-23 12:26:03 +0530

    Mongoose cloud connection, created Modal and Add new Tours

---
+++ nodeAPI/modals/tours.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// console.log(mongoose);
+
+const tourSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    rating: {
+        type: Number,
+        required: false,
+        default: 4.0,
+    },
+    price: {
+        type: Number,
+        required: [true, "A Tour must have a price!"],
+    },
+    premium: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+});
+
+module.exports = Tours = mongoose.model("Tours", tourSchema);
--- nodeAPI/server.js
+++ nodeAPI/server.js
@@ -1,12 +1,33 @@
 const app = require("./app");
-const dotenv = require("dotenv");
-const env = require("./config.env");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Tours = require("./modals/tours");
 
-dotenv.config({ path: "./config.env" });
+const DB = process.env.DATABASE.replace("PASSCODE", process.env.PASSWORD);
+mongoose.connect(DB).then(console.log("Connection Successfull!"));
+
+const newTour = new Tours({
+    name: "The Northen Lights",
+    description:
+        "Enjoy the Northern Lights in one of the best places in the world.",
+    price: 1497,
+    premium: true,
+    rating: 4.9,
+});
+
+newTour
+    .save()
+    .then((doc) => {
+        console.log(doc);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
 const port = process.env.PORT || 3000;
 // Start the server
-console.log(process.env);
-app.listen(3000, () => {
-    console.log(`Server started at http:127.0.0.1:3000`);
-    // console.log(`Server started at ${process.env.HOST}:${port}`);
+// console.log(process.env);
+app.listen(port, () => {
+    // console.log(`Server started at http://127.0.0.1:3000`);
+    console.log(`Server started at ${process.env.HOST}:${port}`);
 });
