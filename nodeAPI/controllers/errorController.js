418cb21be028c23ad6c6fa9567e4affab2b775ef
vantiya <parvezvantiya@gmail.com>
2021-11-27 19:11:21 +0530

    Error Handling with Express - Final

--- nodeAPI/controllers/errorController.js
+++ nodeAPI/controllers/errorController.js
@@ -1,3 +1,24 @@
+const ApiError = require("./../utils/apiError");
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new ApiError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+    console.log(err.errmsg);
+    const val = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(val);
+    const message = `Duplicate field value: ${val}. Please use another value.`;
+    return new ApiError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map((el) => el.message);
+    const message = `Invalide input data. ${errors.join(". ")}`;
+    return new ApiError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -32,6 +53,11 @@ module.exports = (err, req, res, next) => {
         sendErrorDev(err, res);
     } else {
         // if (process.env.NODE_ENV == "production") {
+        let error = { ...err };
+        if (error.name === "CastError") error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === "ValidationError")
+            error = handleValidationErrorDB(error);
         sendErrorProd(err, res);
     }
 };
--- nodeAPI/server.js
+++ nodeAPI/server.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const Tours = require("./modals/tours");
 
+process.on("uncaughtException", (err) => {
+    console.log("UNCAUGHT EXCEPTION! Shutting down...");
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace("PASSCODE", process.env.PASSWORD);
 mongoose.connect(DB).then(console.log("Connection Successfull!"));
 
@@ -12,4 +18,12 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     // console.log(`Server started at http://127.0.0.1:3000`);
     console.log(`Server started at ${process.env.HOST}:${port}`);
+});
+
+process.on("unhandledRejection", (err) => {
+    console.log(err.name, err.message);
+    console.log("Unhandled Rejection! Shutting Down...");
+    server.close(() => {
+        process.exit(1);
+    });
 });
