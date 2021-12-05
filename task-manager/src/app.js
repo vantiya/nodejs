2fa47f6992787363493d02fed35c88c852e678e9
vantiya <parvezvantiya@gmail.com>
2021-12-05 19:38:35 +0530

    CRUD operation - MongoDB & Pure Node

---
+++ task-manager/src/app.js
@@ -0,0 +1,133 @@
+const mongodb = require("mongodb")
+const MongoClient = mongodb.MongoClient
+
+const connectionUrl = "mongodb://127.0.0.1:27017"
+const databaseName = 'task-manager'
+
+// Pure MongoDB and Node connection tests and CRUD operation
+MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+
+    // // Insert ONe
+    
+    // db.collection('users').insertOne({
+    //     name: 'Andrew',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // // Insert Many
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // // Insert Many
+    db.collection('tasks').insertMany([
+        {
+            description: 'Clean the house',
+            completed: true
+        },{
+            description: 'Renew inspection',
+            completed: false
+        },{
+            description: 'Pot plants',
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert tasks!')
+        }
+
+        console.log(result.ops)
+    })
+
+
+    // // Find one Select One by Object id,
+    // db.collection('users').findOne({ _id: new ObjectID("5c1113239cbfe605241f9071") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch')
+    //     }
+
+    //     console.log(user)
+    // })
+
+    // // find by age
+    // db.collection('users').find({ age: 27 }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    // // Find ny object
+    // db.collection('tasks').findOne({ _id: new ObjectID("5c0fec243ef6bdfbe1d62e2f") }, (error, task) => {
+    //     console.log(task)
+    // })
+
+    // // Find ny task status
+    // db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+    //     console.log(tasks)
+    // })
+
+    // // Update One
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID("5c0fe6634362c1fb75b9d6b5")
+    // }, {
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // // Update Many
+    // db.collection('tasks').updateMany({
+    //     completed: false
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }).then((result) => {
+    //     console.log(result.modifiedCount)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // // Delete Many
+    // db.collection('users').deleteMany({
+    //     age: 27
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    // // Delete One
+    db.collection('tasks').deleteOne({
+        description: "Clean the house"
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+})
