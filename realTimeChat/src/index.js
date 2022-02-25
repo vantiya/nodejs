145172bc9c57e1e61664de9294182919c17935ac
vantiya <parvezvantiya@gmail.com>
2022-02-25 11:06:27 +0530

    Setting up socket.io

--- realTimeChat/src/index.js
+++ realTimeChat/src/index.js
@@ -1,12 +1,20 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const pubDirPath = path.join(__dirname, "../public");
 app.use(express.static(pubDirPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("Connected to socket");
+});
+
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
