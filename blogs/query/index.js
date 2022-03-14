0050b264f6c050f38b21e7505aaeadb23af147a6
vantiya <parvezvantiya@gmail.com>
2022-03-14 10:09:17 +0530

    Add/parse/use query service.

---
+++ blogs/query/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    if ('postCreated' === type) {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
+    }
+    if ('CommentCreated' === type) {
+        const { id, content, postId } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+    res.status(200).send({});
+});
+
+app.listen(4002, () => {
+    console.log('Listening on 4002');
+});
