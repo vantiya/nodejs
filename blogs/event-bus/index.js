779a5fedec7cfc272ea6fea188c7ed442529f452
vantiya <parvezvantiya@gmail.com>
2022-03-23 18:44:33 +0530

    Event Sync.

--- blogs/event-bus/index.js
+++ blogs/event-bus/index.js
@@ -7,8 +7,12 @@ const port = process.env.PORT || 4005;
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     axios.post('http://localhost:4000/events', event).catch((err) => {
         console.log(err.message);
@@ -23,6 +27,10 @@ app.post('/events', (req, res) => {
         console.log(err.message);
     });
     res.status(200).send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
 });
 
 app.listen(port, () => {
